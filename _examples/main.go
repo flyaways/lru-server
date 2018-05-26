a66c35134bcf0e1816fb61f3de0d53bbfd17c520
flyaways <meagleglass@gmail.com>
2018-05-27 00:12:57 +0800

    initial commit

---
+++ _examples/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/flyaways/c/simplelru"
+	lru "github.com/flyaways/golang-lru"
+	server "github.com/flyaways/lru-server"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	size    = flag.Int("s", 10000, "-s 10000")
+	port    = flag.String("p", ":8080", "-p :8080")
+	mode    = flag.String("m", "debug", "-m debug/release")
+	cPolicy = flag.Int("t", 1, "-t 0/1/2/3")
+	cache   simplelru.LRUCache
+)
+
+const (
+	sLRU = iota
+	safeLRU
+	twoQ
+	tARC
+)
+
+func NewCache(cPolicy, size int) (simplelru.LRUCache, error) {
+	switch cPolicy {
+	case sLRU:
+		return simplelru.NewLRU(8, func(key interface{}, value interface{}) {
+			fmt.Println(time.Now().Format(time.RFC3339Nano))
+		})
+
+	case safeLRU:
+		/* return lru.NewWithEvict(8, func(key interface{}, value interface{}) {
+			fmt.Println(time.Now().Format(time.RFC3339Nano))
+		}) */
+		return lru.New(size)
+
+	case twoQ:
+		return lru.New2Q(size)
+
+	case tARC:
+		return lru.NewARC(size)
+
+	default:
+		panic(cPolicy)
+	}
+}
+
+func main() {
+	flag.Parsed()
+
+	cache, err := NewCache(*cPolicy, *size)
+	if err != nil {
+		panic(err)
+	}
+
+	gin.SetMode(*mode)
+	engine := gin.New()
+	engine.Use(gin.Logger(), gin.Recovery())
+
+	server.Version1(engine.Group("/api/v1"), cache)
+
+	engine.Run(*port)
+}
---
+++ v1.go
@@ -0,0 +1,182 @@
+package server
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"unsafe"
+
+	"github.com/flyaways/c/simplelru"
+	"github.com/gin-gonic/gin"
+)
+
+//Meta data
+func Meta(v *gin.RouterGroup, cache simplelru.LRUCache) {
+	//Keys
+	v.GET("/keys", func(c *gin.Context) {
+		c.JSON(http.StatusOK, cache.Keys())
+	})
+
+	//Len
+	v.GET("/len", func(c *gin.Context) {
+		c.JSON(http.StatusOK, cache.Len())
+	})
+
+	//Purge
+	v.DELETE("/purge", func(c *gin.Context) {
+		cache.Purge()
+	})
+
+	//GetOldest
+	v.GET("/getoldest", func(c *gin.Context) {
+		cache.GetOldest()
+	})
+
+	//RemoveOldest
+	v.DELETE("/removeoldest", func(c *gin.Context) {
+		cache.RemoveOldest()
+	})
+
+	//Contains
+	v.GET("/contains", func(c *gin.Context) {
+		key := c.Query("key")
+		if key == "" {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		c.JSON(http.StatusOK, cache.Contains(key))
+	})
+
+	//Peek
+	v.GET("/peek", func(c *gin.Context) {
+		key := c.Query("key")
+		if key == "" {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		val, ok := cache.Peek(key)
+		if !ok {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		c.JSON(http.StatusOK, val)
+	})
+
+	//HTTP 0.9 GET
+	//HTTP 1.0 GET HEAD POST
+	//HTTP 1.1 GET HEAD POST OPTIONS PUT DELETE TRACE CONNECT
+	v.OPTIONS("/:key", func(c *gin.Context) {
+		c.Header("Allow", "OPTIONS")
+		c.Header("Allow", "GET")
+		c.Header("Allow", "DELETE")
+	})
+}
+
+//Object ops
+func Object(v *gin.RouterGroup, cache simplelru.LRUCache) {
+	//Get
+	v.GET("/:key", func(c *gin.Context) {
+		key := c.Param("key")
+		if key == "" {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		val, ok := cache.Get(key)
+		if !ok {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		c.JSON(http.StatusOK, val)
+	})
+
+	//Head
+	v.HEAD("/:key", func(c *gin.Context) {
+		key := c.Param("key")
+		if key == "" {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		val, ok := cache.Get(key)
+		if !ok {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		c.Header("Content-Length", strconv.Itoa(int(unsafe.Sizeof(val))))
+	})
+
+	//Add
+	v.PUT("/:key", func(c *gin.Context) {
+		key := c.Param("key")
+		if key == "" {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		data, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		if !cache.Add(key, data) {
+			c.Status(http.StatusInternalServerError)
+		}
+	})
+
+	//Add
+	v.POST("/:key", func(c *gin.Context) {
+		key := c.Param("key")
+		if key == "" {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		data, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		if !cache.Add(key, data) {
+			c.Status(http.StatusInternalServerError)
+		}
+	})
+
+	//Remove
+	v.DELETE("/:key", func(c *gin.Context) {
+		key := c.Param("key")
+		if key == "" {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		if !cache.Remove(key) {
+			c.Status(http.StatusInternalServerError)
+		}
+	})
+
+	//HTTP 0.9 GET
+	//HTTP 1.0 GET HEAD POST
+	//HTTP 1.1 GET HEAD POST OPTIONS PUT DELETE TRACE CONNECT
+	v.OPTIONS("/:key", func(c *gin.Context) {
+		c.Header("Allow", "OPTIONS")
+		c.Header("Allow", "GET")
+		c.Header("Allow", "POST")
+		c.Header("Allow", "PUT")
+		c.Header("Allow", "DELETE")
+		c.Header("Allow", "HEAD")
+	})
+}
+
+//Version1 api version 1
+func Version1(v *gin.RouterGroup, cache simplelru.LRUCache) {
+	Meta(v.Group("/meta"), cache)
+	Object(v.Group("/object"), cache)
+}
